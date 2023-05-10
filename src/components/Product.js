5b078e443c39f231f2822d0e935967a7aef3814e
wefi-dex <www.walter1125@gmail.com>
2023-05-10 10:42:11 -0400

    updated navbar

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,40 +1,44 @@
-import { useEffect, useState } from 'react'
-import { ethers } from 'ethers'
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
 
 // Components
-import Rating from './Rating'
+import Rating from "./Rating";
 
-import close from '../assets/close.svg'
+import close from "../assets/close.svg";
 
 const Product = ({ item, provider, account, dappazon, togglePop }) => {
-  const [order, setOrder] = useState(null)
-  const [hasBought, setHasBought] = useState(false)
+  const [order, setOrder] = useState(null);
+  const [hasBought, setHasBought] = useState(false);
 
   const fetchDetails = async () => {
-    const events = await dappazon.queryFilter("Buy")
+    const events = await dappazon.queryFilter("Buy");
     const orders = events.filter(
-      (event) => event.args.buyer === account && event.args.itemId.toString() === item.id.toString()
-    )
+      (event) =>
+        event.args.buyer === account &&
+        event.args.itemId.toString() === item.id.toString()
+    );
 
-    if (orders.length === 0) return
+    if (orders.length === 0) return;
 
-    const order = await dappazon.orders(account, orders[0].args.orderId)
-    setOrder(order)
-  }
+    const order = await dappazon.orders(account, orders[0].args.orderId);
+    setOrder(order);
+  };
 
   const buyHandler = async () => {
-    const signer = await provider.getSigner()
+    const signer = await provider.getSigner();
 
     // Buy item...
-    let transaction = await dappazon.connect(signer).buy(item.id, { value: item.cost })
-    await transaction.wait()
+    let transaction = await dappazon
+      .connect(signer)
+      .buy(item.id, { value: item.cost });
+    await transaction.wait();
 
-    setHasBought(true)
-  }
+    setHasBought(true);
+  };
 
   useEffect(() => {
-    fetchDetails()
-  }, [hasBought])
+    fetchDetails();
+  }, [hasBought]);
 
   return (
     <div className="product">
@@ -51,7 +55,7 @@ const Product = ({ item, provider, account, dappazon, togglePop }) => {
 
           <p>{item.address}</p>
 
-          <h2>{ethers.utils.formatUnits(item.cost.toString(), 'ether')} ETH</h2>
+          <h2>{ethers.utils.formatUnits(item.cost.toString(), "ether")} ETH</h2>
 
           <hr />
 
@@ -59,60 +63,63 @@ const Product = ({ item, provider, account, dappazon, togglePop }) => {
 
           <p>
             {item.description}
-
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima rem, iusto,
-            consectetur inventore quod soluta quos qui assumenda aperiam, eveniet doloribus
-            commodi error modi eaque! Iure repudiandae temporibus ex? Optio!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima rem,
+            iusto, consectetur inventore quod soluta quos qui assumenda aperiam,
+            eveniet doloribus commodi error modi eaque! Iure repudiandae
+            temporibus ex? Optio!
           </p>
         </div>
 
         <div className="product__order">
-          <h1>{ethers.utils.formatUnits(item.cost.toString(), 'ether')} ETH</h1>
+          <h1>{ethers.utils.formatUnits(item.cost.toString(), "ether")} ETH</h1>
 
           <p>
             FREE delivery <br />
             <strong>
-              {new Date(Date.now() + 345600000).toLocaleDateString(undefined, { weekday: 'long', month: 'long', day: 'numeric' })}
+              {new Date(Date.now() + 345600000).toLocaleDateString(undefined, {
+                weekday: "long",
+                month: "long",
+                day: "numeric",
+              })}
             </strong>
           </p>
 
-          {item.stock > 0 ? (
-            <p>In Stock.</p>
-          ) : (
-            <p>Out of Stock.</p>
-          )}
+          {item.stock > 0 ? <p>In Stock.</p> : <p>Out of Stock.</p>}
 
-          <button className='product__buy' onClick={buyHandler}>
+          <button className="product__buy" onClick={buyHandler}>
             Buy Now
           </button>
 
-          <p><small>Ships from</small> Dappazon</p>
-          <p><small>Sold by</small> Dappazon</p>
+          <p>
+            <small>Ships from</small> Dappazon
+          </p>
+          <p>
+            <small>Sold by</small> Dappazon
+          </p>
 
           {order && (
-            <div className='product__bought'>
+            <div className="product__bought">
               Item bought on <br />
               <strong>
-                {new Date(Number(order.time.toString() + '000')).toLocaleDateString(
-                  undefined,
-                  {
-                    weekday: 'long',
-                    hour: 'numeric',
-                    minute: 'numeric',
-                    second: 'numeric'
-                  })}
+                {new Date(
+                  Number(order.time.toString() + "000")
+                ).toLocaleDateString(undefined, {
+                  weekday: "long",
+                  hour: "numeric",
+                  minute: "numeric",
+                  second: "numeric",
+                })}
               </strong>
             </div>
           )}
         </div>
 
-
         <button onClick={togglePop} className="product__close">
           <img src={close} alt="Close" />
         </button>
       </div>
-    </div >
+    </div>
   );
-}
+};
 
 export default Product;
